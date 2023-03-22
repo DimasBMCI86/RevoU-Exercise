f3d1c318ee72f75eaa37e91054a0ec8f0ba8e6f7
dimasbmci86 <michaelsetiawan9@gmail.com>
2023-03-22 05:45:45 -0700

    revisi ke 3.6

---
+++ BMI Kalkulator/js/index.js
@@ -0,0 +1,46 @@
+const form = document.querySelector('form');
+const result = document.querySelector('#result');
+
+// menambahkan event pada tombol submit
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const height = parseInt(form.elements.height.value);
+    const weight = parseInt(form.elements.weight.value);
+    const gender = form.elements.gender.value;
+    const bmi = calculateBMI(height, weight);
+    const status = getStatus(bmi, gender);
+    result.textContent = `BMI Anda Adalah: ${bmi.toFixed(1)}(${status})`;
+});
+    
+// membuat fungsi kalkulasi
+function calculateBMI(height, weight) {
+    const heightM = height / 100;
+    return weight / (heightM * heightM);
+}
+
+// print hasil status
+function getStatus(bmi, gender) {
+    if (gender === 'male') {
+      if (bmi < 20) {
+        return 'wah anda langsing...';
+    } else if (bmi < 25) {
+        return 'berat anda normal';
+    } else if (bmi < 30) {
+        return 'waduh sepertinya anda sedikit gemuk. itu bagus tapi jangan terlalu gemuk.';      
+    } else {
+        return 'astaga anda kegemukan. harap berolahraga agar badan tetap sehat!';
+        }
+} else {
+    if (bmi < 18) {
+        return 'anda kurus';
+    } else if (bmi < 23) {
+        return 'berat badan normal';
+    } else if (bmi < 28) {
+        return 'anda sedikit gemuk';
+    } else {
+        return 'waduh anda mengalami obesitas. harap berolahraga agar badan tetap sehat!'
+    }
+}
+}
+
+document.getElementsByClassName("form");
